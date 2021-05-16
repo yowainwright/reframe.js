69630d3247b3cc1cbcd6a35daebd3da8a0881e80
yowainwright <yowainwright@gmail.com>
2021-05-15 23:38:52 -0700

    fix: re-add type declarations 😳

---
+++ types/noframe.d.ts
@@ -0,0 +1,1 @@
+export default function noframe(target: string | HTMLElement, container: string): void;
---
+++ types/reframe.d.ts
@@ -0,0 +1,8 @@
+/**
+ * REFRAME.TS 🖼
+ * ---
+ * @param target
+ * @param cName
+ * @summary defines the height/width ratio of the targeted <element>
+ */
+export default function reframe(target: string | NodeList, cName?: string): void;
