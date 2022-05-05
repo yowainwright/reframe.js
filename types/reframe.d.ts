b20138130439d76994f0ecdf7fd93153fe06eb04
Jeff Wainwright <yowainwright@gmail.com>
2022-05-05 15:15:37 -0700

    feat: init release-it (#576)
    * feat: init release-it
    
    * fix: remove types dir

--- types/reframe.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * REFRAME.TS 🖼
- * ---
- * @param target
- * @param cName
- * @summary defines the height/width ratio of the targeted <element>
- */
-export default function reframe(target: any, cName: any): void;
